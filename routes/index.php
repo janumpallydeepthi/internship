18d0003f402d68c5e062bc1cdb4d7bbe0d03319d
janumpallydeepthi <deepthi.janumpally@gmail.com>
2025-06-08 20:05:36 +0530

    New Changes

---
+++ routes/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link ref="stylesheet" src="styles.css">
+    </head>
+    <body>
+        <h2 style="color:red">Hello</h1>
+            <?php
+            $age=45;
+            $name='Rohit';
+            echo "His name is $name, and age is $age";
+            echo "<br>";
+            echo "His name is <b> $name</b>, and age is $age";
+            ?>
+    </body>
+</html>
